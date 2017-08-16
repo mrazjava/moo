e6eb22322e4b8762953c3855292d6e3ed86d917b
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 06:50:20 +0200

    bare client project

---
+++ moo-client/src/main/java/pl/zimowski/moo/client/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Root space for client application of the moo chat service.
+ *
+ * @since 1.0.0
+ * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
+ */
+package pl.zimowski.moo.client;
