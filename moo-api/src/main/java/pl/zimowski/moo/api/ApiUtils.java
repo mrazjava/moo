badd0191d412d982a04471b7f6eebad8b42ca218
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 08:15:22 +0200

    fetchResource: implementation

--- moo-api/src/main/java/pl/zimowski/moo/api/ApiUtils.java
+++ moo-api/src/main/java/pl/zimowski/moo/api/ApiUtils.java
@@ -1,6 +1,16 @@
 package pl.zimowski.moo.api;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ApiUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiUtils.class);
 
     /**
      * Given a resource path, reads the resource and returns its content as {@code String}. Capable
@@ -11,6 +21,24 @@ public class ApiUtils {
      * @return content of a file or {@code null} if resource could not be fetched
      */
     public static String fetchResource(String resourcePath) {
-        return null;
+
+        InputStream is = ApiUtils.class.getClassLoader().getResourceAsStream(resourcePath);
+        if(is == null) is = ApiUtils.class.getResourceAsStream(resourcePath);
+        if(is == null) log.warn("invlid resource: {}", resourcePath);
+
+        String fetchedContent = null;
+
+        if(is != null) {
+            log.debug("loading resource: {}", resourcePath);
+            try {
+                fetchedContent = IOUtils.toString(is, Charset.defaultCharset());
+                log.trace("fetched content:\n{}", fetchedContent);
+            }
+            catch(IOException e) {
+                log.error("problem reading resource [{}]: {}", resourcePath, e.getMessage());
+            }
+        }
+
+        return fetchedContent;
     }
 }
