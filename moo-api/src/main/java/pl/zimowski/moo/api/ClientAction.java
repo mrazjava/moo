66d402eea307ffc023566bc116386f81a7d54bdc
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 07:31:38 +0200

    enumerated client/server events

---
+++ moo-api/src/main/java/pl/zimowski/moo/api/ClientAction.java
@@ -0,0 +1,26 @@
+package pl.zimowski.moo.api;
+
+/**
+ * Possible events client can emit to a server.
+ *
+ * @since 1.0.0
+ * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
+ */
+public enum ClientAction {
+
+    /**
+     * Client established connection to a server with identified end user.
+     * After this operation user is ready to chat.
+     */
+    Signin,
+
+    /**
+     * Client emitted a chat message to a server on behalf of end user.
+     */
+    Message,
+
+    /**
+     * User terminated chat session (client terminated connection).
+     */
+    Signoff
+}
---
+++ moo-api/src/main/java/pl/zimowski/moo/api/ServerAction.java
@@ -0,0 +1,26 @@
+package pl.zimowski.moo.api;
+
+/**
+ * Possible events server can emit to a client.
+ *
+ * @since 1.0.0
+ * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
+ */
+public enum ServerAction {
+
+    /**
+     * report count of online users due to change to client collection (login,
+     * logout, timeout)
+     */
+    ParticipantCount,
+
+    /**
+     * server aborted client connection due to inactivity
+     */
+    ConnectionTimeOut,
+
+    /**
+     * chat message from another user
+     */
+    Message
+}
