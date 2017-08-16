63839c4efa2c944fdcb4105c73353de897b071ca
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 12:29:03 +0200

    server parameters of the client configuration

---
+++ moo-client/src/main/java/pl/zimowski/moo/client/configuration/ServerSettings.java
@@ -0,0 +1,35 @@
+package pl.zimowski.moo.client.configuration;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * Spring managed configuration of server parameters. Getters intentionally
+ * omitted to encourage direct value injection.
+ *
+ * @since 1.0.0
+ * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
+ */
+@Component
+@Validated
+@ConfigurationProperties(prefix = "server")
+public class ServerSettings {
+
+    @NotNull(message = "server host is required")
+    private String host;
+
+    @NotNull(message = "server port is required")
+    private Integer port;
+
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
