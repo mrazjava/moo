a3f7adcbf89df7e1081188df59c0d7ec931adb20
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 08:53:00 +0200

    javadoc

--- moo-server/src/main/java/pl/zimowski/moo/server/ChatService.java
+++ moo-server/src/main/java/pl/zimowski/moo/server/ChatService.java
@@ -1,12 +1,23 @@
 package pl.zimowski.moo.server;
 
 /**
+ * High level operations that can be invoked on a chat service.
+ *
  * @since 1.0.0
  * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
  */
 public interface ChatService {
 
+    /**
+     * Starts chat service. After this operation, remote client should be able
+     * to connect to a service.
+     */
     void start();
 
+    /**
+     * Reports if service is up or down.
+     *
+     * @return {@code true} if service is running, {@code false} otherwise.
+     */
     boolean isRunning();
 }
