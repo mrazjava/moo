ff2eb69cd4ee66e9c08e0bb3557c96e09cd5a4a8
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 07:58:35 +0200

    implementation of client/server events

--- moo-api/src/main/java/pl/zimowski/moo/api/ClientEvent.java
+++ moo-api/src/main/java/pl/zimowski/moo/api/ClientEvent.java
@@ -12,8 +12,18 @@ public class ClientEvent implements Serializable {
 
     private static final long serialVersionUID = -3135034051959415690L;
 
+    private String author;
+
+    private String message;
+
+    private long timestamp;
+
+    private ClientAction action;
+
 
     public ClientEvent(ClientAction action) {
+        timestamp = System.currentTimeMillis();
+        this.action = action;
     }
 
     public ClientEvent(ClientAction action, String author, String message) {
@@ -23,26 +33,34 @@ public class ClientEvent implements Serializable {
     }
 
     public ClientEvent withAuthor(String author) {
+        this.author = author;
         return this;
     }
 
     public ClientEvent withMessage(String message) {
+        this.message = message;
         return this;
     }
 
     public ClientAction getAction() {
-        return null;
+        return action;
     }
 
     public String getAuthor() {
-        return null;
+        return author;
     }
 
     public String getMessage() {
-        return null;
+        return message;
     }
 
     public long getTimestamp() {
-        return 0;
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientEvent [author=" + author + ", message=" + message + ", timestamp=" + timestamp + ", action="
+                + action + "]";
     }
 }
--- moo-api/src/main/java/pl/zimowski/moo/api/ServerEvent.java
+++ moo-api/src/main/java/pl/zimowski/moo/api/ServerEvent.java
@@ -12,35 +12,54 @@ public class ServerEvent implements Serializable {
 
     private static final long serialVersionUID = -6175363790070655216L;
 
+    private long timestamp;
+
+    private ServerAction action;
+
+    private String message;
+
+    private int participantCount;
+
 
     public ServerEvent(ServerAction action) {
+        this.action = action;
+        timestamp = System.currentTimeMillis();
     }
 
     public ServerEvent(ServerAction action, String message) {
         this(action);
+        withMessage(message);
     }
 
     public ServerEvent withMessage(String message) {
+        this.message = message;
         return this;
     }
 
     public ServerEvent withParticipantCount(int count) {
+        participantCount = count;
         return this;
     }
 
     public long getTimestamp() {
-        return 0;
+        return timestamp;
     }
 
     public ServerAction getAction() {
-        return null;
+        return action;
     }
 
     public int getParticipantCount() {
-        return 0;
+        return participantCount;
     }
 
     public String getMessage() {
-        return null;
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return "ServerEvent [timestamp=" + timestamp + ", action=" + action + ", message=" + message
+                + ", participantCount=" + participantCount + "]";
     }
 }
