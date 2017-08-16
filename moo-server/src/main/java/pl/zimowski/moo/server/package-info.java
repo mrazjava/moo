ad2febc565f558eab2f91a30d3dee37b4e261248
Adam Zimowski <adam.zimowski@allpago.com>
2017-08-16 06:56:02 +0200

    bare server project

---
+++ moo-server/src/main/java/pl/zimowski/moo/server/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Root space for the server of the moo chat service.
+ *
+ * @since 1.0.0
+ * @author Adam Zimowski (<a href="mailto:mrazjava@yandex.com">mrazjava</a>)
+ */
+package pl.zimowski.moo.server;
